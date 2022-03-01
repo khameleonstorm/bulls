d9c0e78d11142b3e78f21bc30619b3cea464fc42
victor nwachukwu <khameleonstorm@gmail.com>
2022-03-01 16:17:50 +0100

    pages linking

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('about', views.about, name = 'about'),
+    path('login', views.login, name = 'login'),
+    path('signup', views.signup, name = 'signup'),
+    path('withdraw', views.withdraw, name = 'withdraw'),
+    path('deposit', views.deposit, name = 'deposit'),
+    path('bnb', views.bnb, name = 'bnb'),
+    path('btc', views.btc, name = 'btc'),
+    path('eth', views.eth, name = 'eth'),
+    path('sol', views.sol, name = 'sol'),
+    path('usdt', views.usdt, name = 'usdt'),
+    path('dashboard', views.dashboard, name = 'dashboard'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,33 @@ def index(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def signup(request):
+    return render(request, 'pages/signup.html')
+
+def withdraw(request):
+    return render(request, 'pages/withdraw.html')
+
+def deposit(request):
+    return render(request, 'pages/deposit.html')
+
+def bnb(request):
+    return render(request, 'pages/bnb.html')
+
+def btc(request):
+    return render(request, 'pages/btc.html')
+
+def eth(request):
+    return render(request, 'pages/eth.html')
+
+def sol(request):
+    return render(request, 'pages/sol.html')
+
+def usdt(request):
+    return render(request, 'pages/usdt.html')
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
