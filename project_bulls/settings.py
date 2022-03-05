de611aeb0206279a422bef37ea4d55ec05d7a204
victor nwachukwu <khameleonstorm@gmail.com>
2022-03-05 09:12:43 +0100

    static

--- project_bulls/settings.py
+++ project_bulls/settings.py
@@ -127,8 +127,11 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles ')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project_bulls/static')
+]
 
 
 # messages
